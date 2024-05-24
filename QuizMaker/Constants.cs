6f3fee9ce0ee8c514f8e7fd88d58176f15cf790c
Olayode Yusuff <getlayus@gmail.com>
2024-05-24 20:15:36 +0100

    Completed code

--- QuizMaker/Constants.cs
+++ QuizMaker/Constants.cs
@@ -13,7 +13,5 @@ namespace QuizMaker
         public const char SMALl_LETTER_Y = 'y';
         public const char CAPITAL_LETTER_Y = 'Y';
         public const string KEY_OPTIONS = "Invalid key. Please press A, B, C, D, or E";
-
-
     }
 }
